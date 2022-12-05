5e48a58dc42178abbd1d0529bf66b17c9cef6a54
Wolfleader <james.morgan@iinet.net.au>
2022-12-05 09:16:17 +0800

    day 1 part a done

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -3,24 +3,18 @@ use std::str;
 
 fn main() {
     // read file input as string
-    let mut elves: Vec<i32> = fs::read_to_string("input2.txt")
+    let mut elves: Vec<i32> = fs::read_to_string("input.txt")
         .expect("Should read file")
-        .split_whitespace() // split on space to give each elf
+        .split("\r\n\r\n") // split on space to give each elf
         .map(|s: &str| {
             // for each elf
             s.lines() //   split on new line
-                .map(|c: &str| {
-                    c.parse::<i32>().expect("a valid number") // convert to i32
-                })
+                .filter_map(|l: &str| l.parse::<i32>().ok())
                 .sum() // sum the calories in the array
         })
         .collect::<Vec<i32>>();
 
     elves.sort_by(|a, b| b.cmp(a)); // sort elves by highest number
-
-    // for elf in elves {
-    //     println!("{elf}");
-    // }
 
     // return first element in array
     println!("{}", elves[0]);
