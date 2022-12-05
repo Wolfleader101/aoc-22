1425728df93d68d125107e5ca486a5fe5ef163b4
Wolfleader <james.morgan@iinet.net.au>
2022-12-05 08:52:47 +0800

    working on advent of code

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,3 +1,27 @@
+use std::fs;
+use std::str;
+
 fn main() {
-    println!("Hello, world!");
+    // read file input as string
+    let mut elves: Vec<i32> = fs::read_to_string("input2.txt")
+        .expect("Should read file")
+        .split_whitespace() // split on space to give each elf
+        .map(|s: &str| {
+            // for each elf
+            s.lines() //   split on new line
+                .map(|c: &str| {
+                    c.parse::<i32>().expect("a valid number") // convert to i32
+                })
+                .sum() // sum the calories in the array
+        })
+        .collect::<Vec<i32>>();
+
+    elves.sort_by(|a, b| b.cmp(a)); // sort elves by highest number
+
+    // for elf in elves {
+    //     println!("{elf}");
+    // }
+
+    // return first element in array
+    println!("{}", elves[0]);
 }
