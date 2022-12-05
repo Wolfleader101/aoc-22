c5dce302cd4ba88c5d74c2faf3d29b0700721c86
Wolfleader <james.morgan@iinet.net.au>
2022-12-05 12:26:13 +0800

    day2 part b completed

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -19,43 +19,23 @@ fn main() {
 }
 
 fn player_score(opp: &str, plyer: &str) -> i32 {
-    let mut score = 0;
-    if plyer.contains("X") {
-        score = 1;
-    } else if plyer.contains("Y") {
-        score = 2;
-    } else if plyer.contains("Z") {
-        score = 3;
-    }
-
-    // score += outcome
-    score += outcome(opp, plyer);
-
-    score
-}
-
-fn outcome(opp: &str, plyer: &str) -> i32 {
-    let plyer_loses = HashMap::from([("X", "B"), ("Y", "C"), ("Z", "A")]);
-    let opp_loses = HashMap::from([("A", "Y"), ("B", "Z"), ("C", "X")]);
-
     // A = rock
     // B = paper
     // C = scissors
+    let plyer_loses = HashMap::from([("A", "C"), ("B", "A"), ("C", "B")]);
+    let opp_loses = HashMap::from([("A", "B"), ("B", "C"), ("C", "A")]);
+    let scores = HashMap::from([("A", 1), ("B", 2), ("C", 3)]);
 
-    // X = rock
-    // Y = paper
-    // Z = scissors
-
-    // rock beats scissors -  A beats Z, X beats C
-    // paper beats rock
-    // scissors beats paper
-
-    if opp_loses.get(opp).eq(&Some(&plyer)) {
-        return 6;
+    let mut score: i32 = 0;
+    if plyer.eq("X") {
+        score += *scores.get(*plyer_loses.get(opp).unwrap()).unwrap();
+    } else if plyer.eq("Y") {
+        score += *scores.get(opp).unwrap();
+        score += 3;
+    } else if plyer.eq("Z") {
+        score += *scores.get(*opp_loses.get(opp).unwrap()).unwrap();
+        score += 6;
     }
 
-    if plyer_loses.get(plyer).eq(&Some(&opp)) {
-        return 0;
-    }
-    return 3;
+    score
 }
