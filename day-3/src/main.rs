0c9bdefed0af27e55cc1142db1527aec83a57923
Wolfleader <james.morgan@iinet.net.au>
2022-12-06 15:49:29 +0800

    day3

---
+++ day-3/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    //read file
+    let rucksacks: i32 = fs::read_to_string("input.txt")
+    //split on new line
+    // split string in half
+
+        // loop over each character in string 1 and check if it is in string 2
+        // get the priority from the hashmap for it
+        // add it to the vector
+    // sum the vector
+}
